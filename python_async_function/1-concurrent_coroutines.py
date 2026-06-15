4ce0a449a0cfd284a48bea27e552d34c4dbf7a9a
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-15 10:36:49 +0200

    fixed it maybe

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,12 +2,12 @@
 """Provides a coroutine for spawning concurrent random delay tasks."""
 
 import asyncio
-import importlib
+from typing import List
 
-wait_random = importlib.import_module('0-basic_async_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times and return delays in ascending order."""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = [await task for task in asyncio.as_completed(tasks)]
