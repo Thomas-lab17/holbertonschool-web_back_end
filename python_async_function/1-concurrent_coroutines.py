15ea3cc790d80eff4383b0de24af9e4973949ea8
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-15 10:32:58 +0200

    fixed checker issue

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
-"""
-This module provides a coroutine for spawning multiple
-random delay tasks concurrently.
-"""
+"""Provides a coroutine for spawning concurrent random delay tasks."""
 
 import asyncio
+import importlib
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = importlib.import_module('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list:
-    """
-    Spawn wait_random n times concurrently and
-    return delays in completion order.
-    """
+    """Spawn wait_random n times and return delays in ascending order."""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = [await task for task in asyncio.as_completed(tasks)]
     return delays
