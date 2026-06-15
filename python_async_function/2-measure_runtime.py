1993aa9111b3739db1bc32d16717b2fdbf6deb05
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-15 14:39:18 +0200

    task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for measuring the runtime of wait_n."""
+
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure total execution time for wait_n and return average per call."""
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    return (end - start) / n
