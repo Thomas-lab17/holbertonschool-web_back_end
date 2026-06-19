f26ab297bd18f5abb42c7c2fe4b8cb0df229b51b
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-19 22:34:53 +0200

    ok

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async comprehension coroutine that collects 10 random numbers."""
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collect 10 random numbers using async comprehension and return them."""
+    return [i async for i in async_generator()]
