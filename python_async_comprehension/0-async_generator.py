3b54c8683e6535b4508692c4980b82480092028e
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-19 22:15:29 +0200

    ok

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Async generator that yields random numbers."""
+"""Async generator coroutine that yields 10 random floats between 0 and 10."""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Loop 10 times, wait 1 second, then yield a random number 0-10."""
+    """Loop 10 times, wait 1 sec, yield a random float between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
